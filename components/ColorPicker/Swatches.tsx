1266fd3b5b22ca62bbf6f27d9f221d5002172cfc
Mateja <mateja176@gmail.com>
2021-12-05 21:09:47 +0100

    adds Swatches

---
+++ components/ColorPicker/Swatches.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import tinycolor from 'tinycolor2';
+import { Color } from '../../models/pixels';
+
+const swatches = [
+  'lightblue',
+  'lightcoral',
+  'lightcyan',
+  'lightgoldenrodyellow',
+  'lightgrey',
+  'lightgreen',
+  'lightpink',
+  'lightsalmon',
+  'lightseagreen',
+  'lightskyblue',
+  'lightslategrey',
+  'lightsteelblue',
+  'lightyellow',
+  'black',
+  'white',
+];
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+  },
+  swatch: {
+    height: 20,
+    width: 20,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderStyle: 'solid',
+    marginRight: 4,
+  },
+});
+
+export interface SwatchesProps {
+  onChange: (color: Color) => void;
+}
+
+const Swatches: React.FC<SwatchesProps> = (props) => {
+  return (
+    <ScrollView horizontal contentContainerStyle={styles.wrapper}>
+      {swatches.map((swatch) => (
+        <TouchableWithoutFeedback
+          key={swatch}
+          onPress={() => {
+            props.onChange(tinycolor(swatch).toRgb());
+          }}
+        >
+          <View style={[styles.swatch, { backgroundColor: swatch }]} />
+        </TouchableWithoutFeedback>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default Swatches;
